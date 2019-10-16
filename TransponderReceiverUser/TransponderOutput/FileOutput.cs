1ecba5b69b131e942b63a7f33016a41248a28ead
Frederik Salling Østergaard <201710715@post.au.dk>
2019-10-16 14:17:04 +0200

    Frederik - Changed in FileOutput

--- TransponderReceiverUser/TransponderOutput/FileOutput.cs
+++ TransponderReceiverUser/TransponderOutput/FileOutput.cs
@@ -6,6 +6,9 @@ namespace TransponderOutput
 {
     class FileOutput
     {
-
+        public void Print(string path, string contents)
+        {
+            //System.IO.File.WriteAllText(@path, contents);
+        }
     }
 }
