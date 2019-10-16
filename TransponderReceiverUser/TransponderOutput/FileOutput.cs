eb2381dbfe85a3451efdd22e598ec35bb0e985ae
Frederik Salling Østergaard <201710715@post.au.dk>
2019-10-16 12:46:08 +0200

    Frederik - au588968 - Added IOuput and output classes

---
+++ TransponderReceiverUser/TransponderOutput/FileOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransponderOutput
+{
+    class FileOutput
+    {
+
+    }
+}
---
+++ TransponderReceiverUser/TransponderOutput/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TransponderOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
