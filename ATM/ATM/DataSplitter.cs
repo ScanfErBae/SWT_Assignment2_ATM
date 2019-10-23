1f6c665233f8b2e7eaefe0054a82aded9e68f99e
Jogans <rasmusdv@hotmail.com>
2019-10-23 14:19:39 +0200

    Created new solution for overskuelighed

---
+++ ATM/ATM/DataSplitter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ATM
+{
+    class DataSplitter : IDataSplitter
+    {
+        private ITransponderReceiver _receiver;
+        public DataSplitter(ITransponderReceiver receiver)
+        {
+            this._receiver = receiver;
+            this._receiver.TransponderDataReady += DataSplit;
+        }
+        public event EventHandler<AirplaneArgs> DataReceivedEvent;
+
+        private void DataSplit(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                string[] input = data.Split(';');
+                NewPlaneReceived(input);
+            }
+        }
+
+        protected virtual void OnDataReceivedEvent(AirplaneArgs e)
+        {
+            DataReceivedEvent?.Invoke(this, e);
+        }
+
+        public void NewPlaneReceived(string [] data)
+        {
+            OnDataReceivedEvent(new AirplaneArgs
+            {
+                Tag = data[0], XCoordinate = Int32.Parse(data[1]), YCoordinate = Int32.Parse(data[2]),
+                ZCoordinate = Int32.Parse(data[3]), TimeYear = data[4].Substring(0,3), 
+                TimeMonth = data[4].Substring(4, 5), TimeDay = data[4].Substring(6, 7), 
+                TimeHour = data[4].Substring(8, 9), TimeMinute = data[4].Substring(10, 11), 
+                TimeSecond = data[4].Substring(12, 13), TimeMilliSecond = data[4].Substring(14, 16)
+            });
+        }
+    }
+}
---
+++ ATM/ATM/IDataSplitter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class AirplaneArgs : EventArgs
+    {
+        public string Tag { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public int ZCoordinate { get; set; }
+        public string TimeYear { get; set; }
+        public string TimeMonth { get; set; }
+        public string TimeDay { get; set; }
+        public string TimeHour { get; set; }
+        public string TimeMinute { get; set; }
+        public string TimeSecond { get; set; }
+        public string TimeMilliSecond { get; set; }
+    }
+    interface IDataSplitter
+    {
+        event EventHandler<AirplaneArgs> DataReceivedEvent;
+    }
+}
