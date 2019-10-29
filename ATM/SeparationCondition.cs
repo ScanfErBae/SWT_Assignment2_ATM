66d1562f673992c9216b5a5403be498da710ab3c
Jogans <rasmusdv@hotmail.com>
2019-10-29 18:42:03 +0100

    kommentar

--- ATM/SeparationCondition.cs
+++ ATM/SeparationCondition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace ATM
 {
     public class SeparationCondition : ISeparationCondition
@@ -117,3 +117,4 @@ namespace ATM
     }
 
 }
+*/
