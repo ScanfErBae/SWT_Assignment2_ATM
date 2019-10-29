9186a965e9d4f3c03a10756409ac577454320ad6
salling008 <frederiksalling8@hotmail.com>
2019-10-29 18:24:06 +0100

    Changes in fileoutput

--- ATM/FileOutput.cs
+++ ATM/FileOutput.cs
@@ -14,7 +14,8 @@ namespace ATM
             string content = "";
             content = plane.CurrentTime.ToLongDateString() + " " + plane.CurrentTime.ToLongTimeString() + ":" + plane.CurrentTime.Millisecond + "Plane: " + plane.Tag + " Close to: " + plane.SeparationCond[1] + "\n";
             string path = "";
-        path = @"C:\Users\Frederik\Documents\Uni\SWT\SWT_Assignment2_ATM\ATM\Output.txt";
+            path = @"C:\Users\Frederik\Documents\Uni\SWT\SWT_Assignment2_ATM\ATM\Output.txt";
+            //path = @"C:\Users\Frederik\Documents\Uni\SWT\SWT_Assignment2_ATM\ATM\Output.txt";
             //path = (Directory.GetCurrentDirectory() + @"\Output.txt");
             System.IO.File.AppendAllText(path, content);
         }
