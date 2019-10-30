acea33aca1d1737cbc6446cba04c4030ea37fcc7
Jogans <rasmusdv@hotmail.com>
2019-10-30 09:10:19 +0100

    Overloaded Equals in plane class

--- ATM.Test.Unit/DataSplitter.Test.Unit.cs
+++ ATM.Test.Unit/DataSplitter.Test.Unit.cs
@@ -1,79 +1,80 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Configuration;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using NSubstitute;
-//using NUnit.Framework;
-//using TransponderReceiver;
-//using ATM;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+using ATM;
 
-//namespace ATM.Test.Unit
-//{
-//    class DataSplitterTestUnit
-//    {
+namespace ATM.Test.Unit
+{
+    class DataSplitterTestUnit
+    {
 
-//        private ITransponderReceiver _fakeTransponderReceiver;
-//        private DataSplitter _uut;
-//        private AirplaneArgs receivedArgs;
-//        private int NumberOfEvents;
+        private ITransponderReceiver _fakeTransponderReceiver;
+        private DataSplitter _uut;
+        private AirplaneArgs receivedArgs;
+        private int NumberOfEvents;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            //Make a fake Transponder Data Receiver
-//            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+        [SetUp]
+        public void Setup()
+        {
+            //Make a fake Transponder Data Receiver
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
 
-//            //Inject the fake
-//            _uut = new DataSplitter(_fakeTransponderReceiver);
+            //Inject the fake
+            _uut = new DataSplitter(_fakeTransponderReceiver);
 
-//            receivedArgs = null;
-//            NumberOfEvents = 0;
-
-
-//            _uut.DataReceivedEvent += (s, a) =>
-//            {
-//                receivedArgs = a;
-//                NumberOfEvents++;
-//            };
-//        }
-
-//        [Test]
-//        public void TestReception_ThreeRelevantPlanes()
-//        {
-//            // Setup test data
-//            List<string> testData = new List<string>();
-//            testData.Add("ATR423;39045;12932;14000;20151006213456789");
-//            testData.Add("BCD123;10005;85890;12000;20151006213456789");
-//            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
-
-//            // Act: Trigger the fake object to execute event invocation
-//            _fakeTransponderReceiver.TransponderDataReady
-//                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
-
-//            //Assert
-//            Assert.That(NumberOfEvents, Is.EqualTo(1));
-//        }
-
-//        //[Test]
-//        //public void Data_From_Plane_Is_Split()
-//        //{
-//        //    // Setup test data
-//        //    List<string> testPlane = new List<string>();
-//        //    testPlane.Add("ATR423;39045;12932;14000;20151006213456789");
-
-//        //    //Act: Trigger the fake object to execute event invocation
-//        //    _fakeTransponderReceiver.TransponderDataReady
-//        //        += Raise.EventWith(this, new RawTransponderDataEventArgs(testPlane));
-
-//        //    //Assert
-//        //    Assert.That(receivedArgs._planes, Is.EqualTo(testPlane));
-//        //}
+            receivedArgs = null;
+            NumberOfEvents = 0;
 
 
-//    }
-//}
+            _uut.DataReceivedEvent += (s, a) =>
+            {
+                receivedArgs = a;
+                NumberOfEvents++;
+            };
+        }
+
+        [Test]
+        public void TestReception_ThreeRelevantPlanes()
+        {
+            // Setup test data
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+            // Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            //Assert
+            Assert.That(NumberOfEvents, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Data_From_Plane_Is_Split()
+        {
+            // Setup test data
+            List<string> testPlane = new List<string>();
+            testPlane.Add("ATR423;39045;12932;14000;20151006213456789");
+            Plane planeToCompareTo =
+                new Plane("ATR423", 39045, 12932, 14000, new DateTime(2015, 10, 06, 21, 34, 56, 000));
+            //Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testPlane));
+
+            //Assert
+            Assert.That(receivedArgs._planes[0], Is.EqualTo(planeToCompareTo));
+        }
+
+
+    }
+}
 
 
 
