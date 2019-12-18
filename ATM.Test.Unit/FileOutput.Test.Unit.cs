59e7f9c8b135b80d5cd605e8dbdb340f60189373
salling008 <frederiksalling8@hotmail.com>
2019-12-18 12:03:26 +0100

    Git fucked jenkins

--- ATM.Test.Unit/FileOutput.Test.Unit.cs
+++ ATM.Test.Unit/FileOutput.Test.Unit.cs
@@ -46,5 +46,41 @@ namespace ATM.Test.Unit
             Assert.IsTrue(File.Exists(path));
 
         }
+
+        [TestCase("TestTag")]
+        public void TestPrintContent(string tag)
+        {
+
+            // Act: Trigger the fake object to execute event invocation
+            // Assert something here or use an NSubstitute Received
+            File.Delete(path);
+
+            DateTime time = new DateTime(2019, 10, 29, 15, 55, 40, 200);
+
+            Plane testPlane = new Plane
+            {
+                Tag = tag,
+                CurrentTime = time
+            };
+            testPlane.SeparationCond.Add("ABC1234");
+            // Setup test data
+            _uut.Print(testPlane);
+
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            //Arrange
+
+            string str = sr.ReadLine();
+
+            sr.Close();
+            fs.Close();
+
+            string expected = "Tuesday, 29 October, 2019 Kl: 15:55:40:200 Plane: TestTag Close to: ABC1234";
+
+            StringAssert.Contains(str, expected);
+
+
+        }
     }
 }
